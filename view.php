930d1b6cbce7e36c1467c7d251545f8fe8e70950
juannitoo <jeanbalangue@gmail.com>
2017-12-28 19:07:25 +0100

    passage mvc

---
+++ view.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link href="mybooks.css" rel="stylesheet" />
+    <title>MyBooks - Home</title>
+</head>
+<body>
+    <header>
+        <h1>MyBooks</h1>
+    </header>
+    <?php foreach ($books as $book): ?>
+    <article>
+        <h2><?php echo $book['book_title'] ?></h2>
+        <p><?php echo $book['book_summary'] ?></p>
+    </article>
+    <?php endforeach ?>
+    <footer class="footer">
+        <a href="https://github.com/juannitoo/OC-MyBooks/tree/master" >
+        MyBooks</a> is a minimalistic CMS built as a showcase 
+        for modern PHP development.
+    </footer>
+</body>
+</html>
